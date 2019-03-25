5cc7a163b6addc63ac46a537cbec59f540dc93f4
William St-Arnaud <william.starnaud@gmail.com>
2019-03-25 09:46:50 -0400

    Try without loop

--- assignment2/models.py
+++ assignment2/models.py
@@ -264,7 +264,7 @@ class GRU(nn.Module): # Implement a stacked GRU RNN
 
     def forward(self, inputs, hidden):
         logits = []
-        for t in range(self.seq_len):
+        """for t in range(self.seq_len):
             h_in = self.emb(inputs[t])
             new_hidden = []
 
@@ -283,7 +283,19 @@ class GRU(nn.Module): # Implement a stacked GRU RNN
             logits.append(self.w_y(out))
 
         # logits = torch.stack(logits)
-        logits = torch.cat(logits)
+        logits = torch.cat(logits)"""
+        h_in = self.emb(inputs)
+        h_in = self.dropout(h_in)
+        new_hidden = []
+        for i in range(self.num_layers):
+            r_t = torch.sigmoid(self.w_r[i](h_in) + self.u_r[i](hidden[i]))
+            z_t = torch.sigmoid(self.w_z[i](h_in) + self.u_z[i](hidden[i]))
+            h_tilde = torch.tanh(self.w_h[i](h_in) + self.u_h[i](r_t * hidden[i]))
+            h_t = (1. - z_t) * hidden[i] + z_t * h_tilde
+            new_hidden.append(h_t)
+            h_in = h_t
+        h_in = self.dropout(h_in)
+        logits = self.w_y(h_in)
 
         return logits.view(self.seq_len, self.batch_size, self.vocab_size), hidden
 
